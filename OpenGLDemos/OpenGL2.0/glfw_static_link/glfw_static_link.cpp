e4fb43b718720e4b9cf58196f352c8096060cee7
zdd <vckzdd@gmail.com>
2014-05-29 10:00:30 +0800

    Add project glfw_static_link

---
+++ OpenGLDemos/OpenGL2.0/glfw_static_link/glfw_static_link.cpp
@@ -0,0 +1,53 @@
+/*
+1. For dynamic link, please use glfw3dll.lib and glfw3.dll
+2. For static link, please use glfw3.lib, and set C/C++ - Code Generation - Runtime Library to /Mt or /Mtd
+*/
+
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(480, 320, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Draw a triangle */
+		glBegin(GL_TRIANGLES);
+
+		glColor3f(1.0, 0.0, 0.0);	// Red
+		glVertex3f(0.0, 1.0, 0.0);
+
+		glColor3f(0.0, 1.0, 0.0);	// Green
+		glVertex3f(-1.0, -1.0, 0.0);
+
+		glColor3f(0.0, 0.0, 1.0);	// Blue
+		glVertex3f(1.0, -1.0, 0.0);
+
+		glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
