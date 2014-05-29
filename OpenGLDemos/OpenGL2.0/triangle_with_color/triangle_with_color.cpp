603b59b1b29de7eb24cd67cea3ea4828a303bb0f
zdd <vckzdd@gmail.com>
2014-05-29 10:01:55 +0800

    Correct comments, Read -> Red for color

--- OpenGLDemos/OpenGL2.0/triangle_with_color/triangle_with_color.cpp
+++ OpenGLDemos/OpenGL2.0/triangle_with_color/triangle_with_color.cpp
@@ -7,7 +7,7 @@ void display(void)
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glBegin(GL_TRIANGLES);
-		glColor3f(1.0, 0.0, 0.0);	// Read
+		glColor3f(1.0, 0.0, 0.0);	// Red
 		glVertex3f(0.0, 1.0, 0.0);
 
 		glColor3f(0.0, 1.0, 0.0);	// Green
